55fa26123a9baa0fff4263e0d4083609d5a3c7ad
patrik <ptrk.lechner@gmail.com>
2018-01-14 16:26:46 +0100

    fft

--- code/fourierTransform.py
+++ code/fourierTransform.py
@@ -1,20 +1,28 @@
-import numpy as np
-import matplotlib.pyplot as plt
+import numpy as np #importing a library
+import matplotlib.pyplot as plt #importing a library
 
-sr = 44100.
+sr = 44100. #sample rate
 N = 1024 #length in smaples
 lenSec = N/sr #length in seconds
 
 f = 1000 #frequency of sine wave to generate
-t = np.linspace(0,lenSec, N)
+t = np.linspace(0,lenSec, N) # time array
 x = np.sin(t*2*np.pi*f) #making a sine wave as input signal
 
 xf = np.zeros_like(x,dtype=np.complex_) #initializing complex array of zeros
 
 # Actual Discrete Fourier Transform
-for m in range(N):
-    for n in range(N):
+for m in range(N): #looping over all frequencies
+    for n in range(N): #looping over the whole input signal
         xf[m] = xf[m]+x[n]*np.exp(-1j*2*np.pi*n*m/N)
 
-plt.plot(abs(xf)) #plotting the spectrum (two-sided)
-plt.show()
+#======plotting===================
+plt.subplot(211)
+plt.plot(x) #plotting the input
+plt.xlabel('time [samples]')
+plt.title('input, time domain')
+plt.subplot(212)
+plt.plot(abs(xf)) #plotting the magnitude spectrum (two-sided)
+plt.xlabel('frequency [bins/samples]')
+plt.title('output, frequency domain')
+plt.show() #displaying the output plot
