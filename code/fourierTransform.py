cdbfe21556c437155cecb7fc99cc06dc068c57b3
patrik <ptrk.lechner@gmail.com>
2018-01-14 16:50:50 +0100

    fft

--- code/fourierTransform.py
+++ code/fourierTransform.py
@@ -1,20 +1,21 @@
 import numpy as np #importing a library
 import matplotlib.pyplot as plt #importing a library
-
+pi = np.pi #get pi
+e = np.exp #get e^ function
 sr = 44100. #sample rate
 N = 1024 #length in smaples
 lenSec = N/sr #length in seconds
 
 f = 1000 #frequency of sine wave to generate
 t = np.linspace(0,lenSec, N) # time array
-x = np.sin(t*2*np.pi*f) #making a sine wave as input signal
+x = np.sin(t*2*pi*f) #making a sine wave as input signal
 
-xf = np.zeros_like(x,dtype=np.complex_) #initializing complex array of zeros
+xf = np.zeros_like(x,dtype=np.complex_) #initializing complex output array of zeros
 
 # Actual Discrete Fourier Transform
 for m in range(N): #looping over all frequencies
     for n in range(N): #looping over the whole input signal
-        xf[m] = xf[m]+x[n]*np.exp(-1j*2*np.pi*n*m/N)
+        xf[m] = xf[m]+x[n]*e(-1j*2*pi*n*m/N)
 
 #======plotting===================
 plt.subplot(211)
